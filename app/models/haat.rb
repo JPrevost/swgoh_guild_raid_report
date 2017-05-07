5ffa4d72cb3210e842310fb7fb92eac38fc238f0
Jeremy Prevost <jprevost@mit.edu>
2017-05-07 09:32:16 -0400

    Initial haat model

---
+++ app/models/haat.rb
@@ -0,0 +1,9 @@
+# Determine roster readiness
+class Haat
+  P1_TENACITY = ['Aayla Secura', 'Jedi Knight Anakin', 'Ima-Gun Di', 'Qui-Gon Jinn', 'Ahsoka Tano']
+
+  P1_ZYLO = ['Kylo Ren']
+
+  def team_check(roster, team)
+  end
+end
